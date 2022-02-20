36e0729d73fc9d3bbbbdff4535ac218e79883b53
jeseias <domingosjeseias@gmail.com>
2022-02-20 20:42:28 +0100

    chore: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+import {
+  QueryClientProvider
+} from 'react-query'
+
+import { queryClient } from '@/presentation/modules/query-client'
 
 import Router from '@/main/routes/router'
 import { theme } from '@/presentation/styles/global-styles'
-import { BrowserRouter } from 'react-router-dom'
 import { Wrapper } from './presentation/components'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <Wrapper>
-          <Router />
-        </Wrapper>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <Wrapper>
+            <Router />
+          </Wrapper>
+        </ChakraProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,8 +1,13 @@
+import { LoadClinics } from '@/domain/usecases'
 import { Box } from '@chakra-ui/react'
 
 import { HomeClinicsSection, HomeHeaderSection, HomeServicesSection } from './components'
 
-export const HomePage = () => {
+type Props = {
+  loadClinics: LoadClinics
+}
+
+export const HomePage = ({ loadClinics }: Props) => {
   return (
     <Box alignItems="center">
       <HomeHeaderSection />
