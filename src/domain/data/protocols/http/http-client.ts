1674963c7b34b5b1091882fdc0cf6f776d455417
jeseias <domingosjeseias@gmail.com>
2022-02-20 12:12:49 +0100

    feat: add HttpClient interface

---
+++ src/domain/data/protocols/http/http-client.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+export type HttpMethod = 'post' | 'get'
+
+export enum HttpStatusCode {
+  ok = 200,
+  notFound = 404
+}
+
+export type HttpRequest = {
+  url: string
+  method: HttpMethod
+  body?: any
+  headers?: any
+}
+
+export type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode
+  body?: T
+}
+
+export interface HttpClient<R = any> {
+  request(data: HttpRequest): Promise<HttpResponse<R>>
+}
