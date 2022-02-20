f0041f60e3ddcbdda6b7e8228ef4b1eb622c3538
jeseias <domingosjeseias@gmail.com>
2022-02-20 13:10:28 +0100

    feat: ensure AxiosHttpClient calls axios with correct values

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,33 @@
+import { mockAxios, mockHttpRequest } from '@/__mocks__'
+import axios from 'axios'
+import { AxiosHttpClient } from '..'
+
+jest.mock('axios')
+
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return {
+    sut,
+    mockedAxios
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  it('Should call axios with correct values', async () => {
+    const request = mockHttpRequest()
+    const { sut, mockedAxios } = makeSut()
+    await sut.request(request)
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      url: request.url,
+      data: request.body,
+      headers: request.headers,
+      method: request.method
+    })
+  })
+})
