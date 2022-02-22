654385089ed888335caf1d6c9809d51d8c701b80
jeseias <domingosjeseias@gmail.com>
2022-02-22 09:30:29 +0100

    feat: use cache first while fetching most current data

--- src/presentation/pages/clinic-page/clinic-page.tsx
+++ src/presentation/pages/clinic-page/clinic-page.tsx
@@ -1,10 +1,12 @@
 import { Box, Heading, Text } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import {
-  useQuery
+  useQuery, useQueryClient
 } from 'react-query'
 
 import { LoadClinicById } from '@/domain/usecases'
+import { Clinic } from '@/domain/models'
+import { useEffect, useState } from 'react'
 
 type ClinicPageParamProps = {
   id: string
@@ -17,7 +19,24 @@ type Props = {
 export const ClinicPage = ({ loadClinicById }: Props) => {
   const { id } = useParams() as ClinicPageParamProps
 
-  const { data: clinic, isFetched, isLoading } = useQuery('clinic', async () => await loadClinicById.load(id))
+  const [clinic, setClinic] = useState<Clinic | null>(null)
+
+  const { data } = useQuery(['clinic', id], async () => await loadClinicById.load(id))
+
+  const queryClient = useQueryClient()
+
+  useEffect(() => {
+    const clinics = queryClient.getQueryData<Clinic[]>('clinics')
+
+    if (data) {
+      return setClinic(data)
+    }
+
+    if (clinics) {
+      const currentClinic = clinics.filter(item => item.id === id)[0]
+      return setClinic(currentClinic)
+    }
+  }, [data])
 
   return (
     <Box>
@@ -29,30 +48,24 @@ export const ClinicPage = ({ loadClinicById }: Props) => {
         bgSize="cover"
         bgPos="center"
       />
-      {isLoading && <p>Loading</p>}
-      {isFetched && (
-        <Box maxWidth="142rem" width="100%" m="0 auto">
-          <Box bg="whiteAlpha.900">
-            <Heading
-              fontSize="6rem"
-              position="relative"
-              top="-3.6rem"
-              color="whiteAlpha.900"
-              textShadow="0 0 .4rem rgba(0,0,0, .8)"
-              p="0 4rem"
-            >
-              {clinic?.name}
-            </Heading>
-            <Box mt="5rem" p="5rem" fontSize="4rem">
-              <Text><b>Address:</b> {clinic?.address}</Text>
-              <Text><b>CPF:</b> {clinic?.cpf}</Text>
-            </Box>
-          </Box>
-          <Box bg="blackAlpha.200">
-            <Heading>Available Services</Heading>
+      <Box maxWidth="142rem" width="100%" m="0 auto">
+        <Box bg="whiteAlpha.900">
+          <Heading
+            fontSize="6rem"
+            position="relative"
+            top="-3.6rem"
+            color="whiteAlpha.900"
+            textShadow="0 0 .4rem rgba(0,0,0, .8)"
+            p="0 4rem"
+          >
+            {clinic?.name}
+          </Heading>
+          <Box mt="5rem" p="5rem" fontSize="4rem">
+            <Text><b>Address:</b> {clinic?.address}</Text>
+            <Text><b>CPF:</b> {clinic?.cpf}</Text>
           </Box>
         </Box>
-      )}
+      </Box>
     </Box>
   )
 }
